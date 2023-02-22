c033289e2f3ac010ed7b537e0074a0c66df627d8
PavelKaminski <sdminvest@gmail.com>
2023-02-22 18:28:08 +0300

    Initial commit

---
+++ src/test/java/com/kaminski/overone/pet_shop/PetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaminski.overone.pet_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
