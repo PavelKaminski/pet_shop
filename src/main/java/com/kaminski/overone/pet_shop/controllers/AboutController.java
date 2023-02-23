a592cdbe510200e20359726896a40f4ae455f4e9
PavelKaminski <sdminvest@gmail.com>
2023-02-24 01:11:37 +0300

    DB properties added Main page and about page controllers added Views of main page and about page added

---
+++ src/main/java/com/kaminski/overone/pet_shop/controllers/AboutController.java
@@ -0,0 +1,16 @@
+package com.kaminski.overone.pet_shop.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/about")
+public class AboutController {
+
+    @GetMapping
+    public String getAboutPage() {
+        return "about";
+    }
+}
